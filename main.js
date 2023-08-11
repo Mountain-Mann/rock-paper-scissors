6f6eca905562eb0214b1defc3e3d1f8e9388deba
MountainMan <colemanrumsey@gmail.com>
2023-08-11 13:20:47 +0300

    FINALLY FINISHED IT ALL (except CSS)/Reload button works and all logic works now

--- main.js
+++ main.js
@@ -1,75 +1,90 @@
-let compScore = 0;
-let playerScore = 0;
-
-//declare array choices
-const choices = ['rock', 'paper', 'scissors'];
-
-//get computer choice by random
-function getCompChoice () {
-   selection = choices[Math.floor(Math.random()*choices.length)];
-   return selection;
+const playerText = document.querySelector('#playerText');
+const compText = document.querySelector('#compText');
+const resultText = document.querySelector('#resultText');
+const choiceBtns = document.querySelectorAll('.choiceButton');
+const score = document.querySelector('.score');
+const reloadBtn = document.querySelector('.reload');
+let scoreboard = {
+    player: 0,
+    computer: 0
 }
+let computer;
+let player;
+let results;
 
-const computerSelection = getCompChoice();
-
-const rock = document.querySelector('#btn');
-const paper = document.querySelector('#btn2');
-const scissors = document.querySelector('#btn3');
-
-let playerSelection;
-
-rock.addEventListener('click', () => {
-    playerSelection = 'rock';
-    playRound(playerSelection, computerSelection);
-});
-paper.addEventListener('click', () => {
-    playerSelection = 'paper';
-    playRound(playerSelection, computerSelection);
-});
-scissors.addEventListener('click', () => {
-    playerSelection = 'scissors';
-    playRound(playerSelection, computerSelection);
-});
+choiceBtns.forEach(button => button.addEventListener('click', () => {
+    
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent = `Player: ${player}`;
+    compText.textContent = `Computer: ${computer}`;
+    resultText.textContent = `Result: ${checkWinner()}`;
+    score.innerHTML = `
+    <p>Player: ${scoreboard.player}</p>
+    <br>
+    <p>Computer: ${scoreboard.computer}</p>
+    `;
+}));
 
 
-const results = document.querySelector('#results');
+function computerTurn(){
 
-//declare function to play a round
-function playRound (playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        results.textContent = 'It\'s a tie...';
-    } else if (
-        (playerSelection === 'rock' && computerSelection === 'paper') || 
-        (playerSelection === 'paper' && computerSelection === 'scissors') || 
-        (playerSelection === 'scissors' && computerSelection === 'rock')
-        ) {results.textContent = `Eh, ${(computerSelection)} beats ${playerSelection}! You lose...`;
-    } else if (
-        (playerSelection === 'rock' && computerSelection === 'scissors') || 
-        (playerSelection === 'paper' && computerSelection === 'rock') || 
-        (playerSelection === 'scissors' && computerSelection === 'paper')
-        ) {results.textContent = `Sweet, ${(playerSelection)} beats ${computerSelection}! You win...`;
-    } else {
-        results.textContent = 'Please try again...';
+    const randNum = Math.floor(Math.random() * 3 + 1)
+
+    switch(randNum){
+        case 1:
+            computer = 'Rock';
+            break;
+        case 2:
+            computer = 'Paper';
+            break;
+        case 3:
+            computer = 'Scissors';
+            break;
     }
 };
 
-//playRound(playerSelection, computerSelection);
 
-/*
-for (i = 0; i < 5; i++) {
-    playRound();
-    if (results.textContent == `Eh, ${(computerSelection)} beats ${playerSelection}! You lose...`) {
-        compScore = ++compScore;
-    } else if (results.textContent == `Sweet, ${(playerSelection)} beats ${computerSelection}! You win...`) {
-        playerScore = ++playerScore;
-    } else if (results.textContent = 'It\'s a tie...') {
-        //no points
+function checkWinner(){
+    if((computer == 'Rock') && (player == 'Paper')){
+        scoreboard.player++;
+        return 'You win!';
     }
-}
-*/
+    else if((computer == 'Rock') && (player == 'Scissors')){
+        scoreboard.computer++;
+        return 'You lose!';
+    }
+    else if((computer == 'Paper') && (player == 'Scissors')){
+        scoreboard.player++;
+        return 'You win!';
+    }
+    else if((computer == 'Paper') && (player == 'Rock')){
+        scoreboard.computer++;
+        return 'You lose!';
+    }
+    else if((computer == 'Scissors') && (player == 'Rock')){
+        scoreboard.player++;
+        return 'You win!';
+    }
+    else if((computer == 'Scissors') && (player == 'Paper')){
+        scoreboard.computer++;
+        return 'You lose!';
+    }
+    else {
+        return 'Draw!';
+    }
+};
 
-const container = document.querySelector('#container');
 
-const score = document.createElement('div');
-score.classList.add('score');
-score.textContent = '`The score is ${} to ${}`';
+reloadBtn.addEventListener('click', (e) => {
+    playerText.textContent = `Player: ${''}`;
+    compText.textContent = `Computer: ${''}`;
+    resultText.textContent = 'Result: ';    
+    
+    score.innerHTML = `
+        <p>Player: ${scoreboard.player * 0}</p>
+        <br>
+        <p>Computer: ${scoreboard.player * 0}</p>
+        `;
+});
+
